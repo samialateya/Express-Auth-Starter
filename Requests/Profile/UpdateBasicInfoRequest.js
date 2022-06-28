cc10fc8018d73e886158f8b8865784ce39288ba5
Sami Alateya <samialateya@hotmail.com>
2022-06-28 15:14:59 +0400

    update basic info

--- Requests/Profile/UpdateBasicInfoRequest.js
+++ Requests/Profile/UpdateBasicInfoRequest.js
@@ -3,9 +3,9 @@ const { body } = require('express-validator');
 //form fields validation
 const UpdateBasicInfoRequest = [
 	//name field is required and must be a valid name
-	body('name').isEmpty().withMessage('Name is not valid'),
+	body('name').optional().isString().withMessage('Name is not valid'),
 	//update basic info
-	body('password').isEmpty().isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
+	body('password').optional().isLength({ min: 6 }).withMessage('Password must be at least 6 characters long'),
 ];
 
 module.exports = UpdateBasicInfoRequest;
